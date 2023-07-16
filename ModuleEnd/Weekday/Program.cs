1ad2c5cb0269858990009733462e42df4d926cf7
MonikaSri0701 <128149159+MonikaSri0701@users.noreply.github.com>
2023-07-16 13:18:03 +0530

    Add files via upload

---
+++ ModuleEnd/Weekday/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Weekday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            // LINQ query to select the days of the week.
+            var days = from day in daysOfWeek select day;
+
+            // Iterate through the days of the week
+            foreach (var day in days)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
